ae8a018bd697a49c2d449a69ee6778b62a1cd019
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2023-05-07 08:41:59 -0300

    Update base.js

--- status/base.js
+++ status/base.js
@@ -1,7 +1,9 @@
 // import 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js'
-import Japi from 'status/japi.js'
-import 'status/lanyard.js'
-import 'status/timezone.js'
+const dirname = location.href;
+
+import Japi from dirname + 'status/japi.js'
+import dirname + 'lanyard.js'
+import dirname + 'timezone.js'
 
 const japi = new Japi()
 
